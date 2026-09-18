6fdc204efb861c4e6e0dc49e2375b4935ba2e321
Krut Patel <krutpatel156@gmail.com>
2026-09-18 17:10:27 +0530

    test: remove unused Supabase test import

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -1,7 +1,6 @@
 import 'package:flutter_test/flutter_test.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:shared_preferences/shared_preferences.dart';
-import 'package:supabase_flutter/supabase_flutter.dart';
 import 'package:farmer_market_app/app.dart';
 
 void main() {
